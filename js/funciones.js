3ea41ee44c8a81dabf88765987550dee3e4e63c2
Marta <martatorrens@icloud.com>
2025-06-18 10:40:55 +0200

    version mejorada

--- js/funciones.js
+++ js/funciones.js
@@ -1,17 +1,14 @@
 const navegacion = document.querySelector("nav")
 const botones = document.querySelectorAll(".abrir,.cerrar");
 
-
-botones[0].addEventListener("click", function(){
-    navegacion.className = "desplegado"
-})
-
-botones[1].addEventListener("click", function(){
-    navegacion.className = ""
-})
-
+/*
 for(let i = 0; i < botones.length; i++){
     botones[i].addEventListener("click", function(){
     navegacion.className = i == 0 ? "desplegado" : ""
     })
 }
+ */
+
+botones.forEach( boton => {
+    boton.addEventListener("click", () => navegacion.classList.toggle("desplegado"))
+})
